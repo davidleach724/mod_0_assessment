cc9194913da0ae78dcafc96757c20b7bf5a6800b
David Leach <davidleach724@gmail.com>
2021-04-20 12:25:11 -0500

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,12 @@
+var famousPerson = "John Coltrane";
+var isActive = false;
+famousPerson = "Charlie Parker";
+
+num3 = 21;
+num9 = 13;
+var sum = num3 + num9;
+
+var animals = ['donkey','cat', 'ogre'];
+animals.shift();
+
+console.log(famousPerson.length);
