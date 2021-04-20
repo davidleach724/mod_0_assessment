ee8f57aac83596b4db22437ee14bccdda390cae4
David Leach <davidleach724@gmail.com>
2021-04-20 12:35:21 -0500

    Add airport object

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var airport = {
+  name: "Will Rogers World Airport",
+  city: "Oklahoma City",
+  elevationFeet: 1295,
+  terminals: 24,
+  isBusy: false,
+  hasBar: true,
+  passengerAirlines: ['Alaska', 'Fronteir', 'Southwest', 'Delta'],
+  cargoAirlines: ['FedEx', 'Martinaire', 'UPS']
+}
