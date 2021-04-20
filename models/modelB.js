f4626b6f1b2162db30021945abd3ace8257428a8
David Leach <davidleach724@gmail.com>
2021-04-20 12:39:26 -0500

    Add vehicle object

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var vehicle = {
+  make: "Honda",
+  model: "Odyssey",
+  mileage: 200000,
+  year: 2008,
+  isCool: true,
+  isFast: false,
+  audioOptions: ["CD", "Radio", "Input Jack"],
+  drivers: ["David", "Kiel", "Marcus"]
+}
